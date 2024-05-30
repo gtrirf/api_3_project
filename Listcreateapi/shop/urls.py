4004e89deefb062a590b5f801719c18757d29ac1
gtrirf <131025716+gtrirf@users.noreply.github.com>
2024-05-30 15:43:25 +0500

    version1.0

--- Listcreateapi/shop/urls.py
+++ Listcreateapi/shop/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import CreateProductListView, ListProductView
 
 urlpatterns = [
-    path('create', CreateProductListView.as_view(), name='create'),
+    path('create-or-list/', CreateProductListView.as_view(), name='create'),
     path('list/<int:pk>/', ListProductView.as_view(), name='list'),
 ]
